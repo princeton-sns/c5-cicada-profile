b57794e0b44bf311297282c51cc2d73d0339e30c
Jeffrey Helt <jmhelt@users.noreply.github.com>
2021-12-07 10:58:57 -0500

    Update CopyCat-Cicada experimental setup.

--- profile.py
+++ profile.py
@@ -1,5 +1,5 @@
 """
-CloudLab profile for Cicada
+CloudLab profile for CopyCat-Cicada experiments
 """
 
 # Import the Portal object.
@@ -8,7 +8,7 @@ import geni.portal as portal
 import geni.rspec.pg as pg
 
 # CONSTANTS
-IMAGE = "urn:publicid:IDN+wisc.cloudlab.us+image+cops-PG0:copycat-cicada-node-0"
+IMAGE = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD"
 HW_TYPE = "c220g5"
 
 # Create a portal context.
@@ -21,9 +21,6 @@ request = pc.makeRequestRSpec()
 node = request.RawPC("node")
 node.disk_image = IMAGE
 node.hardware_type = HW_TYPE
-
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
